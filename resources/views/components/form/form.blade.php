2fa69588dbc9899001df89ad1c483d75e24a74c3
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-05-28 12:40:38 +0200

    WiP

--- resources/views/components/form/form.blade.php
+++ resources/views/components/form/form.blade.php
@@ -1,12 +1,22 @@
-<div>
-    <h4>{{ $aura->summary }}</h4>
-    <h5>{{ $aura->description }}</h5>
-    <table>
+<form {{ $attributes->only(['id', 'action']) }} method="POST">
+    @method('PUT')
+    @if (! isset($title) || $title->isEmpty())
+        <h4>{{ $aura->summary }}</h4>
+        <h5>{{ $aura->description }}</h5>
+    @else
+        {{ $title }}
+    @endif
+    <table {{ $attributes->except(['id', 'action']) }}>
         @foreach ($aura->properties as $property)
-        <tr>
-            <th>{{ $property->description }}</th>
-            <td><x-model-editor::form.input :$property /></td>
-        </tr>
+            <tr>
+                <th>{{ $property->description }}</th>
+                <td><x-model-editor::form.input :$property /></td>
+            </tr>
         @endforeach
     </table>
-</div>
+    @if (! isset($buttons) || $buttons->isEmpty())
+        <button type="submit">Save</button>
+    @else
+        {{ $buttons }}
+    @endif
+</form>
