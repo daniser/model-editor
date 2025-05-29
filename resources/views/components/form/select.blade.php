a6f6b7cef7e54e8510940d331710816b4d550978
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-05-29 14:21:16 +0200

    WiP

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -1,8 +1,10 @@
+@use(function Illuminate\Support\enum_value)
+
 @aware(['model'])
 @props(['property'])
 
 <select {{ $attributes }} name="{{ $property->variableName }}" @disabled(! $property->writable)>
     @foreach ($property->type->name::cases() as $case)
-        <option value="{{ $case->value }}" @selected($case->value === $model->{$property->variableName}->value)>{{ $case->value }}</option>
+        <option value="{{ enum_value($case) }}" @selected(enum_value($case) === enum_value($model->{$property->variableName}))>{{ enum_value($case) }}</option>
     @endforeach
 </select>
