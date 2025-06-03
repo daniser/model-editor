e8293af4c249c94d14e17b0f499b46a629ee2b24
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-06-03 10:27:10 +0200

    WiP

--- resources/views/components/form/file.blade.php
+++ resources/views/components/form/file.blade.php
@@ -1,4 +1,12 @@
 @aware(['model'])
 @props(['property'])
 
-<input {{ $attributes }} type="file" name="{{ $property->variableName }}" value="{{ $model->{$property->variableName} }}" {!! $property->type->name === 'list' ? 'multiple' : '' !!} @readonly(! $property->writable) />
+<input {{ $attributes }}
+    type="file"
+    name="{{ $property->variableName }}"
+    @isset($property->type->parameters[0])
+    accept="{{ Str::unwrap($property->type->parameters[0], '"') }}"
+    @endisset
+    {!! $property->type->name === 'list' ? 'multiple' : '' !!}
+    @readonly(! $property->writable)
+/>
--- resources/views/components/form/image.blade.php
+++ resources/views/components/form/image.blade.php
@@ -1,2 +1,10 @@
-<img src="{{ $preview }}" alt="" />
-<input {{ $attributes }} type="file" name="{{ $property->variableName }}" value="{{ $model->{$property->variableName} }}" {!! $property->type->name === 'list' ? 'multiple' : '' !!} @readonly(! $property->writable) />
+<img src="{{ $preview }}" alt="{{ basename($model->{$property->variableName}) }}" title="{{ basename($model->{$property->variableName}) }}" />
+<input {{ $attributes }}
+    type="file"
+    name="{{ $property->variableName }}"
+    @isset($property->type->parameters[0])
+    accept="{{ Str::unwrap($property->type->parameters[0], '"') }}"
+    @endisset
+    {!! $property->type->name === 'list' ? 'multiple' : '' !!}
+    @readonly(! $property->writable)
+/>
