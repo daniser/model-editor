e9ad0111631cc1cc9f535b7cf9aa7f3679bed494
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-07-29 12:02:16 +0200

    WiP

--- src/Contracts/ActionHandler.php
+++ src/Contracts/ActionHandler.php
@@ -8,5 +8,11 @@ use Illuminate\Http\Request;
 
 interface ActionHandler
 {
+    /**
+     * @template T of object
+     *
+     * @param  T  $object
+     * @return T
+     */
     public function update(Request $request, object $object): object;
 }
