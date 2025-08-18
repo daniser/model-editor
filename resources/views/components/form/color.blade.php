6dfed54fee01d07a6a29da21dff9c7c89c041cd1
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-08-18 17:40:20 +0200

    WiP

--- resources/views/components/form/color.blade.php
+++ resources/views/components/form/color.blade.php
@@ -1,4 +1,8 @@
 @aware(['object'])
-@props(['property'])
+@props(['property', 'default' => false])
 
-<input {{ $attributes }} type="color" name="{{ $property->variableName }}" value="{{ $object->{$property->variableName} }}" @readonly(! $property->writable) />
+@if ($default)
+    <span {{ $attributes }} title="{{ $property->defaultValue }}" style="padding: 0 8px; border: 1px solid black; background-color: {{ $property->defaultValue }}"></span>
+@else
+    <input {{ $attributes }} type="color" name="{{ $property->variableName }}" value="{{ $object->{$property->variableName} }}" @readonly(! $property->writable) />
+@endif
