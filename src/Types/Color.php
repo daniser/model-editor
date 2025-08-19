adb027c583f6b5aeca462f35fa9c7099fd291f1b
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-08-19 12:13:34 +0200

    WiP

--- src/Types/Color.php
+++ src/Types/Color.php
@@ -5,14 +5,20 @@ declare(strict_types=1);
 namespace TTBooking\ModelEditor\Types;
 
 use Illuminate\Contracts\Database\Eloquent\Castable;
+use JsonSerializable;
 use Stringable;
 use TTBooking\ModelEditor\Casts\AsColor;
 
-class Color implements Castable, Stringable
+class Color implements Castable, JsonSerializable, Stringable
 {
     public function __construct(public string $value) {}
 
     public function __toString(): string
+    {
+        return $this->value;
+    }
+
+    public function jsonSerialize(): string
     {
         return $this->value;
     }
