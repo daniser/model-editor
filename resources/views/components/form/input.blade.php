7b6e72fc9dfffb93cf63c152194c621d6894ad9f
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-08-19 16:02:20 +0200

    WiP

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,11 +1,11 @@
 @aware(['alias', 'editable'])
 
 @if (! $object && ! $property->hasDefaultValue)
-    <i {{ $attributes }}>{{ __('model-editor::form.na') }}</i>
+    <i {{ $attributes->except('id') }}>{{ __('model-editor::form.na') }}</i>
 @elseif (! $object && $property->defaultValue === null)
-    <i {{ $attributes }}>{{ __('model-editor::form.null') }}</i>
+    <i {{ $attributes->except('id') }}>{{ __('model-editor::form.null') }}</i>
 @elseif ($object && ! $editable && $object->{$property->variableName} === null)
-    <i {{ $attributes }}>{{ __('model-editor::form.null') }}</i>
+    <i {{ $attributes->except('id') }}>{{ __('model-editor::form.null') }}</i>
 @else
     <x-dynamic-component {{ $object && $editable ? $attributes : $attributes->except('id') }} :$component :$property />
 @endif
