81070845a8996975173b76a8d9db40b50ea75b6a
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-08-21 19:07:54 +0200

    WiP

--- resources/views/components/form/file.blade.php
+++ resources/views/components/form/file.blade.php
@@ -1,12 +1,22 @@
-@aware(['object'])
+@use(function TTBooking\ModelEditor\Support\prop_val)
+
+@aware(['object', 'action', 'editable'])
 @props(['property'])
 
-<input {{ $attributes }}
-    type="file"
-    name="{{ $property->variableName }}"
-    @isset($property->type->parameters[0])
-    accept="{{ Str::unwrap($property->type->parameters[0], '"') }}"
-    @endisset
-    {!! $property->type->name === 'list' ? 'multiple' : '' !!}
-    @readonly(! $property->writable)
-/>
+@if ($action)
+    <a {{ $attributes }} href="{{ $action.'/'.$property->variableName }}">{{ prop_val($property, $object) }}</a>
+@else
+    <span {{ $attributes }}>{{ prop_val($property, $object) }}</span>
+@endif
+
+@if ($object && $editable)
+    <input {{ $attributes }}
+        type="file"
+        name="{{ $property->variableName }}"
+        @isset($property->type->parameters[0])
+        accept="{{ Str::unwrap($property->type->parameters[0], '"') }}"
+        @endisset
+        {!! $property->type->name === 'list' ? 'multiple' : '' !!}
+        @readonly(! $property->writable)
+    />
+@endif
--- resources/views/components/form/form.blade.php
+++ resources/views/components/form/form.blade.php
@@ -2,7 +2,7 @@
 
 <form {{ $attributes->only(['id', 'action', 'enctype']) }} method="POST">
     @method('PUT')
-    <x-model-editor::form.table {{ $attributes->except(['id', 'action', 'enctype']) }} :$object :$showDefaults :editable="true" />
+    <x-model-editor::form.table {{ $attributes->except(['id', 'enctype']) }} :$object :$showDefaults :editable="true" />
     @if (! isset($buttons) || $buttons->isEmpty())
         <button type="submit">{{ __('model-editor::form.save') }}</button>
     @else
